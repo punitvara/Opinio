817c542bf3e11da25c2dd24bbcd6ebf4fd02a0eb
Punit Vara <pvara@levi.com>
2023-06-18 22:41:22 +0530

    Create question form to submit question and send post request to backend Working code.

--- UI-app/opinio/src/App.js
+++ UI-app/opinio/src/App.js
@@ -1,22 +1,29 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import QuestionList from './components/QuestionList';
+import CreateQuestionPage from './components/CreateQuestionPage';
 
 const App = () => {
-    const [questions] = useState([
-        { description: "Do you like programming?", yesPrice: 100, noPrice: 50 },
-        { description: "Do you enjoy learning new technologies?", yesPrice: 200, noPrice: 75 }
-    ]);
+  const [questions] = useState([
+    { description: "Do you like programming?", yesPrice: 100, noPrice: 50 },
+    { description: "Do you enjoy learning new technologies?", yesPrice: 200, noPrice: 75 }
+  ]);
 
-    const handleAnswer = (index, answer) => {
-        console.log(`Question ${index + 1} answered ${answer ? "YES" : "NO"}`);
-    };
+  const handleAnswer = (index, answer) => {
+    console.log(`Question ${index + 1} answered ${answer ? "YES" : "NO"}`);
+  };
 
-    return (
-        <div className="App">
-            <QuestionList questions={questions} onAnswer={handleAnswer} />
-        </div>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<QuestionList questions={questions} onAnswer={handleAnswer} />} />
+          <Route path="/create-question" element={<CreateQuestionPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
---
+++ UI-app/opinio/src/components/CreateQuestionPage/CreateQuestionPage.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+import styles from './CreateQuestionPage.module.css';
+
+const CreateQuestionPage = () => {
+  const [formData, setFormData] = useState({
+    question: '',
+    initial_yes_price: '',
+    initial_no_price: '',
+    liquidity_quantity: '',
+    question_type: '',
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Send POST request to localhost:8000/create_question
+    fetch('http://localhost:8000/create_question', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Handle successful response
+          console.log('Question created successfully!');
+        } else {
+          // Handle error response
+          console.error('Failed to create question.');
+        }
+      })
+      .catch((error) => {
+        // Handle any errors during the request
+        console.error('Error creating question:', error);
+      });
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1>Create Question</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.formGroup}>
+          <label htmlFor="question">Question:</label>
+          <input
+            type="text"
+            id="question"
+            name="question"
+            value={formData.question}
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="initial-yes-price">Initial Yes Price:</label>
+          <input
+            type="number"
+            id="initial-yes-price"
+            name="initial_yes_price"
+            value={formData.initial_yes_price}
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="initial-no-price">Initial No Price:</label>
+          <input
+            type="number"
+            id="initial-no-price"
+            name="initial_no_price"
+            value={formData.initial_no_price}
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="liquidity-quantity">Liquidity Quantity:</label>
+          <input
+            type="number"
+            id="liquidity-quantity"
+            name="liquidity_quantity"
+            value={formData.liquidity_quantity}
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="question-type">Question Type:</label>
+          <input
+            type="text"
+            id="question-type"
+            name="question_type"
+            value={formData.question_type}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateQuestionPage;
